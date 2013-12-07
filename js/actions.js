29e132a8421b2f053112ca64ad950365a6dcdbb7
Ryan Koven <celeritas@ucla.edu>
2013-12-06 22:31:40 -0800

    Add prize actions

--- js/actions.js
+++ js/actions.js
@@ -35,3 +35,93 @@ var die = function(){
 		$('#test').css('top', "800px");
 	}, 1000);   
 };
+
+var prize1Action = function(){
+	
+	var current_height = document.getElementById('progress').style.height;
+	var current_top = document.getElementById('progress').style.top;
+	var wrongAnswers = [];
+	for (i in games[puzzle].puzzle){
+		//console.log(i);
+		//console.log(games[puzzle]["puzzle"][i]);
+		if(games[puzzle]["puzzle"][i] == 0){ 
+			wrongAnswers.push(i);
+		}
+	}
+	for (var k = 0; k < 3; k++){
+		var removeWrong = Math.floor(Math.random()*wrongAnswers.length);
+		while (!(removeWrong in wrongAnswers)){
+			removeWrong = (removeWrong + 1)%wrongAnswers.length;
+		}
+		for (var i = 0; i < rows; i++){
+			for (var j = 0; j < cols; j++){
+				if ($('div#' + i + "r" + j).text() == wrongAnswers[removeWrong])
+					$('div#' + i + "r" + j).text(" ");
+			}
+		}
+		delete wrongAnswers[removeWrong];
+	}
+	return 1;
+}
+var prize2Action = function(){
+	//clearInterval(clock);
+	clearInterval(blink);
+	blinking = false;
+	$('#timer').css("visibility", "visible");
+	$('#timer').css("color", "#5dfc0a");
+	$('#timer').css("background-color", "black");
+	setTimeout(function(){
+		$('#timer').css("color", "black");
+		$('#timer').css("background-color", "#5dfc0a");
+	}, 1250);
+	setTime(ten_minutes, (minutes += ((ten_seconds + 3) >= 6) ? 1 : 0), Math.floor((ten_seconds + 3)%6), seconds);
+	return 2;
+};
+var prize3Action = function(){
+	document.getElementById("badGuy1").src = "ufo_blue.jpg";
+	isBlue = true;
+	setTimeout(function(){
+		if (!badGuyDead){
+			document.getElementById("badGuy1").src = "ufo.jpg";
+			isBlue = false;
+		}
+	}, 10000);
+	return 3;
+};
+var prizeActions = [prize1Action, prize2Action, prize3Action];
+/*
+var addPrizeClickHandler = function(prizeClicks){
+	var j = 0;
+	for (var i = 0; i < prizeClicks; i++){
+		$('#prizeClick' + (i + 1)).click(function(){
+			$('div#prize' + (++j)).css("visibility", "visible");
+		});
+	}
+}(3);
+*/
+$('#prizeClick1').click(function(){
+			$('div#prize1').css("visibility", "visible");
+		});
+
+$('#prizeClick2').click(function(){
+			$('div#prize2').css("visibility", "visible");
+		});
+
+$('#prizeClick3').click(function(){
+			$('div#prize3').css("visibility", "visible");
+		});
+
+var addClickHandler = function(i, j){
+	 $('div#' + i.toString() + "r" + j.toString()).click(function(){
+		if (spacePos == (i.toString() + j.toString())){
+			if (chewable){
+				chew();
+				//evalSound('nom');
+				munchCheck();
+			}
+		}
+		else{
+			moveSpaceship(i.toString() + j.toString(), "test");
+		}
+	});
+};
