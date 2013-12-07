5601a587778138124040ba13acad010442561d75
Ryan Koven <celeritas@ucla.edu>
2013-12-06 22:25:42 -0800

    Add games.js

---
+++ js/games.js
@@ -0,0 +1,18 @@
+var games = {
+
+	"kitties": {},
+
+	"dogs" : {"title": "Doggies", "puzzle": {"Daisy": 1, "Caesar": 0, "Julia": 0, "Jack": 1, 
+	"Jet": 1, "Carmen": 0, "kitty": 0, "david koven": 0, "dog": 1,
+	"Roll on John": 1, "Shelter from the Storm": 1, "If You See Her Say Goodbye": 1, "Joker Dog": 0, "The Dog in Me": 0,
+	 "Boots of Spanish Leather": 1, "Who Killed Davie Koven": 0, "Just Like a Robot": 0, "The Spanish Harlem Incident": 1,
+	"Mr. Tambourine<br /> Dog": 0, "Lassie": 1}
+	},
+
+	"bob" : {"title": "Bob Dylan Songs", "puzzle":{"Song for Woody": 1, "It's All Over Now Davie Blue": 0, "Just Like a Woman": 1, "Not Dark Yet": 1, 
+	"The Tornado": 0, "Positively 4th Street": 1, "Live and Let Die": 0, "Neal Young Heart of Gold": 0, "Live Minus Zero/No Limit": 1,
+	"Roll on John": 1, "Shelter from the Storm": 1, "If You See Her Say Goodbye": 1, "Joker Dog": 0, "The Dog in Me": 0,
+	 "Boots of Spanish Leather": 1, "Who Killed Davie Koven": 0, "Just Like a Robot": 0, "The Spanish Harlem Incident": 1,
+	"Mr. Tambourine Dog": 0, "Kodachrome": 0}
+	} 
+};	
